c4d6561daa701ecb832194c335ee3ddaeb05a289
Anderson Nascimento <anderson.nascimento@meta.com.br>
2025-01-19 12:51:06 -0300

    add todo

--- src/http/getTodo.ts
+++ src/http/getTodo.ts
@@ -6,7 +6,13 @@ export interface ITodo {
 
 export async function getTodo():  Promise<ITodo[] | undefined> {
   try {
-    const response = await fetch('http://localhost:3000/todos');
+    const response = await fetch('https://6331c51ecff0e7bf70f62b2c.mockapi.io/todo', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      cache: 'no-cache',
+    });
     const data: ITodo[] = await response.json();
     return data;
   } catch(error) {
